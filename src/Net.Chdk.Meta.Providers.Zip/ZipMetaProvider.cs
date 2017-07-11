8a352bc3855fc54395526abab285804b2f9f8414
Dmitry Shechtman <damnian@gmail.com>
2017-07-11 03:56:19 +0300

    Add product name

--- src/Net.Chdk.Meta.Providers.Zip/ZipMetaProvider.cs
+++ src/Net.Chdk.Meta.Providers.Zip/ZipMetaProvider.cs
@@ -11,46 +11,46 @@ namespace Net.Chdk.Meta.Providers.Zip
         where T : class
     {
         protected ILogger Logger { get; }
-
-        private string FileName { get; }
+        private IBootMetaProvider BootProvider { get; }
 
         protected ZipMetaProvider(IBootMetaProvider bootProvider, ILogger logger)
         {
+            BootProvider = bootProvider;
             Logger = logger;
-            FileName = bootProvider.FileName;
         }
 
-        protected IEnumerable<T> GetItems(string path)
+        protected IEnumerable<T> GetItems(string path, string productName)
         {
+            var bootFileName = BootProvider.GetFileName(productName);
             using (var stream = File.OpenRead(path))
             {
                 var name = Path.GetFileName(path);
-                return GetItems(stream, name);
+                return GetItems(stream, name, bootFileName);
             }
         }
 
-        private IEnumerable<T> GetItems(Stream stream, string name)
+        private IEnumerable<T> GetItems(Stream stream, string name, string bootFileName)
         {
             using (var zip = new ZipFile(stream))
             {
-                return GetItems(zip, name).ToArray();
+                return GetItems(zip, name, bootFileName).ToArray();
             }
         }
 
-        private IEnumerable<T> GetItems(ZipFile zip, string name)
+        private IEnumerable<T> GetItems(ZipFile zip, string name, string bootFileName)
         {
             Logger.LogInformation("Enter {0}", name);
             foreach (ZipEntry entry in zip)
             {
-                var items = GetItems(zip, entry);
+                var items = GetItems(zip, entry, bootFileName);
                 foreach (var item in items)
                     yield return item;
-                yield return GetItem(zip, name, entry);
+                yield return GetItem(zip, name, entry, bootFileName);
             }
             Logger.LogInformation("Exit {0}", name);
         }
 
-        private IEnumerable<T> GetItems(ZipFile zip, ZipEntry entry)
+        private IEnumerable<T> GetItems(ZipFile zip, ZipEntry entry, string bootFileName)
         {
             if (!entry.IsFile)
                 return Enumerable.Empty<T>();
@@ -62,16 +62,16 @@ namespace Net.Chdk.Meta.Providers.Zip
             var name = Path.GetFileName(entry.Name);
             using (var stream = zip.GetInputStream(entry))
             {
-                return GetItems(stream, name);
+                return GetItems(stream, name, bootFileName);
             }
         }
 
-        private T GetItem(ZipFile zip, string name, ZipEntry entry)
+        private T GetItem(ZipFile zip, string name, ZipEntry entry, string bootFileName)
         {
             if (!entry.IsFile)
                 return null;
 
-            if (!FileName.Equals(entry.Name, StringComparison.OrdinalIgnoreCase))
+            if (!bootFileName.Equals(entry.Name, StringComparison.OrdinalIgnoreCase))
                 return null;
 
             return DoGetItem(zip, name, entry);
